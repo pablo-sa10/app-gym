41372a2a38cc6d09deeb4268efbade64e7530531
Pablo Roberto <pablomoura2016@gmail.com>
2024-06-10 16:57:36 -0300

    aaaaaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:fitness/pages/home.dart';
 import 'package:fitness/pages/treino_form.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/model/exercicio.dart
+++ lib/model/exercicio.dart
@@ -1,8 +1,32 @@
-class Exercicio{
+class Exercicio {
+  int id;
+  int treinoId;
   String nome;
   int series;
   int repeticoes;
 
-  Exercicio({required this.nome, required this.series, required this.repeticoes});
+  Exercicio({
+    required this.id,
+    required this.treinoId,
+    required this.nome,
+    required this.series,
+    required this.repeticoes,
+  });
+
+  factory Exercicio.fromMap(Map<String, dynamic> map) {
+    return Exercicio(
+      id: map['id'],
+      treinoId: map['treinoId'],
+      nome: map['nome'],
+      series: map['serie'],
+      repeticoes: map['repeticoes'],
+    );
+  }
+
+  Map<String, dynamic> toMap(){
+    return{
+
+    };
+  }
 
 }
